4137e6b036177bd1e2c6a0c034e0ec82c72e821c
João Martins <joaom8610@gmail.com>
2025-10-16 20:17:12 -0300

    Bootstrap

--- src/pages/loja/loja.jsx
+++ src/pages/loja/loja.jsx
@@ -1,10 +1,21 @@
 
+import Produto from "../../components/produto/produto";
 export default function Loja () {
     
     return (
+        <div className="container">
+            <Produto
+                imagem={<img  alt="Imagem"  src="https://placehold.co/300x200/black/white/png/?text=prod 1"/>}
+                titulo={"prod 1"} 
+                valor={100} 
+                estrela1
+                estrela2
+                estrela3
+                estrela4
+                estrela5
+                />
+        </div>
 
-        <>
-            <h1>Olá lohinha</h1>
-        </>
+        
     );
 }
