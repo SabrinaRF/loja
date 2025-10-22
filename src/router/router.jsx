628c6526eccbb60934634eda650989bfd27fad27
João Martins <joaom8610@gmail.com>
2025-10-21 21:16:22 -0300

    Rota erro 404

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,9 +1,15 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, Route, Routes } from 'react-router-dom';
 import Loja from '../pages/loja/loja.jsx';
+import NotFound from '../components/NotFound/NotFound.jsx';
 
 export const router = createBrowserRouter([
     {
         path: '/',
         element: <Loja />
+    },
+
+    {
+        path: '*',
+        element: <NotFound />
     }
 ]);
